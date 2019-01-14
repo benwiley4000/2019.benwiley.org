fcfc2f49e3faa968aa06925e93f346986ad19d37
Ben Wiley <therealbenwiley@gmail.com>
2019-01-13 22:41:50 -0500

    Navbar supports mobile screen sizes.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,19 +2,10 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const navbarWidth = 180
-const navbarMargin = 30
-
 const NavBar = ({ navItems }) => (
-  <nav
-    style={{
-      position: 'absolute',
-      width: navbarWidth,
-      left: 0 - (navbarWidth + navbarMargin)
-    }}
-  >
+  <nav className="main_nav">
     {navItems.map(({ label, url }) =>
-      <div key={label}>
+      <div className="nav_item" key={label}>
         <Link
           to={url}
           getProps={({ isCurrent }) => isCurrent ? { className: 'current' } : null}
