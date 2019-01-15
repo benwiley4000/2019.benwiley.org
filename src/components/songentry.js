26d3e66237c5882d308026326cb9bbb745976672
Ben Wiley <therealbenwiley@gmail.com>
2019-01-15 01:51:48 -0500

    Add handle to music page progress bars..

--- src/components/songentry.js
+++ src/components/songentry.js
@@ -1,11 +1,21 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import { PlayPauseButton, MediaProgress } from '@cassette/player'
+import { playerContextFilter } from '@cassette/core'
+import { PlayPauseButton } from '@cassette/player'
+import { MediaProgressBar } from '@cassette/components'
+
+function convertToTime(number) {
+  const mins = Math.floor(number / 60);
+  const secs = (number % 60).toFixed();
+  return `${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+const progressBarHandle = <div className="song_progress_handle"><div /></div>;
 
 class SongEntry extends PureComponent {
   render() {
-    const { track, trackIndex, active } = this.props
+    const { track, trackIndex, active, currentTime, duration } = this.props
     return (
       <div className="song_entry">
         <h3>
@@ -14,7 +24,15 @@ class SongEntry extends PureComponent {
         </h3>
         <p>{track.meta.description}</p>
         <div className={'song_entry_progress' + (active ? ' active' : '')}>
-          <MediaProgress />
+          <div className="info">
+            {convertToTime(currentTime)} / {convertToTime(duration)}
+          </div>
+          <MediaProgressBar
+            progressDirection="right"
+            className="progress_bar"
+            progressClassName="progress"
+            handle={progressBarHandle}
+          />
         </div>
       </div>
     )
@@ -25,6 +43,8 @@ SongEntry.propsTypes = {
   track: PropTypes.object.isRequired,
   trackIndex: PropTypes.number.isRequired,
   active: PropTypes.bool.isRequired,
+  currentTime: PropTypes.number.isRequired,
+  duration: PropTypes.number.isRequired
 }
 
-export default SongEntry
+export default playerContextFilter(SongEntry, ['currentTime', 'duration'])
